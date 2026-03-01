6fb25b14ae6635be5d0d954bc1ca11558a858c1a
SPARTAN <gigi.onyegbula.2007@gmail.com>
2026-02-28 18:49:10 -0800

    Began styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import JoinPage from "./pages/JoinPage";
 import ChatPage from "./pages/ChatPage";
 import { FaHome } from "react-icons/fa";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-	  	<h1>
+	  	<header className="header">
 			<a href="/">
-				<FaHome />
+				<h1>
+					<FaHome />
+					TheyChat
+				</h1>
 			</a>
-		</h1>
+			<p>Simple real-time chat app built with React and Socket.IO</p>
+			<p>Created by <a href="https://github.com/GFrosh">DevGideon</a></p>
+		</header>
       <Routes>
-        <Route path="/" element={<JoinPage />} />
-        <Route path="/chat/:room" element={<ChatPage />} />
+        <Route path="/" element={<JoinPage className="join-page" />} />
+        <Route path="/chat/:room" element={<ChatPage className="chat-page" />} />
       </Routes>
     </BrowserRouter>
   );
